7e0e3caf2d1b90c23b75fdca8dc715706be88e84
pianattero <mariapianattero@gmail.com>
2024-01-17 18:52:26 +0100

    Basic structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>HOME</h1>
 </template>
+
+  <script setup lang="ts">
+  </script>
