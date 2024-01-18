2b096c8bdb66dcda423f77290ca059ae8f143099
pianattero <mariapianattero@gmail.com>
2024-01-18 09:09:41 +0100

    Created potion interface - missing data from API

---
+++ src/services/HPService.ts
@@ -0,0 +1,15 @@
+// import { AxiosResponse } from "axios";
+// import { PotionsArray, PotionData } from '@/types/potions.interface'
+// import axios from 'axios';
+
+// export class HPService {
+//     public potionsList: PotionData[] = [];
+
+//     private serviceUrl: string = 'https://api.potterdb.com/v1/potions'
+
+//     constructor(){
+        
+//         public SearchPotionsArray = async (url: string): Promise<AxiosResponse> => {
+//             return await axios.get(url)
+//     }
+// }
